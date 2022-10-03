7943edab6ee85f1c2d3f10645e303dcd5e67811d
FrankBV94 <frankbotelle@gmail.com>
2022-10-03 12:08:40 -0400

    OK

---
+++ router-protected/src/App.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Admin, Analytics, Dashboard, Home, Landing } from "./pages";
+
+function App() {
+  const [user, setuser] = useState(null);
+  const login = () => {
+    setuser({
+      id: 1,
+      name: "John",
+      permissions: ["analize"],
+      roles: ["admin"],
+    });
+  };
+
+  const logout = () => {
+    setuser(null);
+  };
+  return (
+    <BrowserRouter>
+      <Navigation />
+      {user ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <button onClick={login}>Login</button>
+      )}
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route element={<ProtectedRoute isAllowed={!!user} />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route
+          path="/analitycs"
+          element={
+            <ProtectedRoute
+              isAllowed={!!user && user.permissions.includes("analize")}
+              redirectTo="/home"
+            >
+              <Analytics />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute
+              isAllowed={!!user && user.roles.includes("admin")}
+              redirectTo="/home"
+            >
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/landing">Lading</Link>
+        </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/analitycs">Analitycs</Link>
+        </li>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default App;
---
+++ router-protected/src/pages/index.jsx
@@ -0,0 +1,19 @@
+export const Landing = () => {
+  return <h2>Landing Page (Public)</h2>;
+};
+
+export const Home = () => {
+  return <h2>Home Page (Private)</h2>;
+};
+
+export const Dashboard = () => {
+  return <h2>Dashboard Page (Private)</h2>;
+};
+
+export const Analytics = () => {
+  return <h2>Analitycs Page (Private, Permission)</h2>;
+};
+
+export const Admin = () => {
+  return <h2>Admin Page (Private, Permission)</h2>;
+};
